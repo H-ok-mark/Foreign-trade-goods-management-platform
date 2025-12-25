01e520cdc37733f4ad40b86ad176e716917dfa4b
H-ok-mark <2633865300@qq.com>
2025-12-25 16:05:15 +0800

    安装组件

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from '@/router'
 
+app.use(router)
+
+
+app.use(ElementPlus)
 createApp(App).mount('#app')
